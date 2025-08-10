69a243e4b5203832fdfd2fb9b57af08c6266d06a
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-10 17:27:50 +0500

    register login and logout controllers

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -68,7 +68,7 @@ export const login = async(req,res)=>{
         res.json({success:false})
     }
 }
-export const loginOut = async(req,res)=>{
+export const logOut = async(req,res)=>{
     try {
         res.clearCookie('token',{
              httpOnly:true,
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import authRouter from './routes/authRoutes.js'
 const app= express()
 const port = process.env.PORT||3000;
 app.use(express.json())
@@ -13,4 +14,5 @@ connectDB()
 app.get("/",(req,res)=>{
     res.send("api working")
 })
+app.use('/api/auth',authRouter)
 app.listen(port,()=>console.log(`server start listen`))
