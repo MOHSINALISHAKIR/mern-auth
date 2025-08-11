a738a45441eff3f37624bf0bf5e74224737b92e8
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-11 10:51:19 +0500

    middleware for user auth

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+const userAuth = async (req,res,next) => {
+    const { token } = req.cookies;
+    if( !token ){
+        return res.json({success:false,message:'token not found'})
+    }
+    try {
+        const tokenDecode = jwt.verify(token,process.env.JWT_SECRET)
+        if(tokenDecode.id){
+            req.body.userId = tokenDecode.id;
+
+        }else{
+            return res.json({success:false,message:'not authorized user'})
+        }
+        next()
+        
+    } catch (error) {
+        res.json({success:false,message:error.message})
+        
+    }
+}
